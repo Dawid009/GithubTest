a853e1c80074288b38d26f126fb1896f3b5df729
Dawid <wadowicowin@gmail.com>
2024-05-03 19:50:04 +0200

    zmiana 1

--- GithubTest/GithubTest.cpp
+++ GithubTest/GithubTest.cpp
@@ -2,10 +2,13 @@
 //
 
 #include <iostream>
-
+using namespace std;
 int main()
 {
     std::cout << "Hello World!\n";
+    if(1==1){
+        cout << endl;
+    }
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
