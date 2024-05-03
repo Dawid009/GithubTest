32d32a7f4ba87a569f300013350e6fb3f0de1c2d
Dawid009 <dawidknura@gmail.com>
2024-05-03 20:37:12 +0200

    test

--- GithubTest/GithubTest.cpp
+++ GithubTest/GithubTest.cpp
@@ -6,6 +6,10 @@ using namespace std;
 int main()
 {
     std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
     if(1==1){
         cout << endl;
     }
