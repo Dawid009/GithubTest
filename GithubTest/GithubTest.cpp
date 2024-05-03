262bb494bb64286ccda454668413fcfdfcd0ed80
Dawid <wadowicowin@gmail.com>
2024-05-03 20:24:13 +0200

    zmiana 3

--- GithubTest/GithubTest.cpp
+++ GithubTest/GithubTest.cpp
@@ -1,11 +1,17 @@
-// GithubTest.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n" << std::endl;
+    cout << "Hello World!\n" << endl;
+    cout << "Hello World!\n";
+    int x{0};
+    if(1==1){
+        cout << endl;
+        x++;
+        cout << x;
+    }
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
